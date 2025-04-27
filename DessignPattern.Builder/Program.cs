655bd7f02194ecc4baeaee5abcc0cc2258ce8849
serkanyurttapan <serkan.yurttapan@farmasi.com.tr>
2025-04-27 19:42:50 +0300

    Initial commit

---
+++ DessignPattern.Builder/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+var hello = "hello";
+var sb = new StringBuilder();
+sb.Append("<p>");
+sb.Append(hello);
+sb.Append("</p>");
+Console.WriteLine(sb.ToString());
+
+var words = new[] { "hello", "world" };
+sb.Clear();
+sb.Append("<ul>");
+foreach (var word in words)
+{
+    sb.Append(string.Format("<li>{0}</li>", word));
+}
+
+sb.Append("</ul>");
+
+Console.WriteLine(sb.ToString());
+
+
+public class HtmlElement
+{
+    public string Name, Text;
+    public IList<HtmlElement> HtmlElements = new List<HtmlElement>();
+    private const int indentSize = 2;
+
+    public HtmlElement()
+    {
+    }
+
+    public HtmlElement(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+
+    private string ToStringImpl(int indent)
+    {
+        var sb = new StringBuilder();
+        var i = new string(' ', indentSize * indent);
+        sb.Append($"{i}<{Name}>");
+        if (!string.IsNullOrWhiteSpace(Text))
+        {
+            sb.Append(new string(' ', indentSize * (indent + 1)));
+            sb.AppendLine(Text);
+        }
+
+        foreach (var element in HtmlElements)
+        {
+            sb.Append(element.ToStringImpl(indent + 1));
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ FluentBuilder/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/*
+* FluentBuilder pattern : Bir nesnenin adım adım(ve okunabilir şekilde) oluşturulmasını sağlayan bir tasarım desenidir.
+* Özelliği şu; Metotlar birbirlerine zincirlenerek (method chaining) çağrılır ve nesne inşası daha okunaklı,
+daha kontrollü hale gelir.
+ */
+
+using System.Text.Json;
+
+var order = new Order.Builder().SetCustomerName("serkan").SetAddress("icerenkoy").Build();
+
+Console.WriteLine(JsonSerializer.Serialize(order));
+
+public class Order
+{
+    public string CustomerName { get; private set; }
+    public string Address { get; private set; }
+    public string Note { get; private set; }
+    public string ShippingMethod { get; private set; }
+    
+    private Order() {}
+    
+    public class Builder
+    {
+        private readonly Order _order = new Order();
+
+        public Builder SetCustomerName(string name)
+        {
+            _order.CustomerName = name;
+            return this;
+        }
+
+        public Builder SetAddress(string address)
+        {
+            _order.Address = address;
+            return this;
+        }
+
+        public Order Build()
+        {
+            return _order;
+        }
+    }
+
+
+}
